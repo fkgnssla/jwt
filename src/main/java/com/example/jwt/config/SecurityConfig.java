46f4b1c5e743574d1c588e7ed8319e526c222bb1
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-03-22 17:02:07 +0900

    jwt를 위한 security 설정

---
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.jwt.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+// https://github.com/spring-projects/spring-security/issues/10822 참고
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+public class SecurityConfig {
+
+    @Autowired
+    private CorsConfig corsConfig;
+
+    //인증이 없을때는 controller에 @CrossOrigin() 사용 가능, 인증이 있을때는 필터에 등록해줘야 한다.
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //session 사용 X
+                .and()
+                .formLogin().disable() //Form 로그인 방식 사용 X
+                .httpBasic().disable() //Http 로그인 방식 사용 X
+                .authorizeRequests(authroize -> authroize.antMatchers("/api/v1/user/**")
+                        .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") //user 페이지 => 모든 role 가능
+                        .antMatchers("/api/v1/manager/**") //manager 페이지 => manager, admin 만 가능
+                        .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                        .antMatchers("/api/v1/admin/**") //admin 페이지 => admin만 가능
+                        .access("hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll()) //이외 모든 페이지는 모두 허락
+                .build();
+    }
+
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(corsConfig.corsFilter());
+        }
+    }
+}
